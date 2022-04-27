976f0a6974e59d9c241fbb8b2f69fc018bbc3ad9
Jacob Cowan <Mrjcowman@gmail.com>
2022-04-26 20:05:31 -0700

    Implement basic tile manipulation

--- SomethingFromNothing/Assets/SomethingFromNothing/Gameplay/HexGrid.cs
+++ SomethingFromNothing/Assets/SomethingFromNothing/Gameplay/HexGrid.cs
@@ -2,14 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using SomethingFromNothing;
+
 public class HexGrid : MonoBehaviour
 {
+    public HexTile pfHexTile;
     GridLayout gridLayout;
+    Dictionary<Vector2Int, HexTile> tiles;
 
     // Start is called before the first frame update
     void Start()
     {
         gridLayout = transform.GetComponent<GridLayout>();
+        tiles = new Dictionary<Vector2Int, HexTile>();
+
+        // TODO: populate the game start state
+        CreateEmptyTile(new Vector2Int(0,0));
+
+        SFNGame.ResetScore();
     }
 
     // Update is called once per frame
@@ -18,9 +28,21 @@ public class HexGrid : MonoBehaviour
         // Get clicked coordinates
         if (Input.GetMouseButtonDown(0)) {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Debug.Log(string.Format("Mouse Coordinates: ({0}, {1})", pos.x, pos.y));
-            Vector3Int cellPos = gridLayout.WorldToCell(pos);
-            Debug.Log(string.Format("Cell Coordinates: [{0}. {1}]", cellPos.x, cellPos.y, cellPos.z));
+
+            Vector2Int cellPos = (Vector2Int) gridLayout.WorldToCell(pos);   // conversion drops the z
+            
+            // If there is an open space in thatt position, place the tile
+            if (tiles.ContainsKey(cellPos) && tiles[cellPos].IsEmpty()) {
+                tiles[cellPos].Place();
+                Debug.Log("Valid position!");
+            } else {
+                Debug.Log("Invalid position!");
+            }
         }
+    }
+
+    void CreateEmptyTile(Vector2Int cellPos) {
+        Vector3 worldPos = gridLayout.CellToWorld((Vector3Int)cellPos);
+        tiles[cellPos] = Instantiate(pfHexTile, worldPos, Quaternion.identity);
     }
 }
